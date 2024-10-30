d4d3f63609b20446d09a1c039e36ee5c1c54cbf6
srimant0203 <srimant.acharjya@gmail.com>
2024-10-30 11:08:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,14 @@ import searchRoutes from "./routes/search.route.js";
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 import { protectRoute } from "./middleware/protectRoute.js";
-import job from "./cron/cron.js";
+//import job from "./cron/cron.js";
 
 const app = express();
 
 
 const PORT = ENV_VARS.PORT;
 const __dirname = path.resolve();
-job.start();
+//job.start();
 
 app.use(express.json()); // will allow us to parse req.body
 app.use(cookieParser());
